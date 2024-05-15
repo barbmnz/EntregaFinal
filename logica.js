a41486bcaaa4213230cb58744a44d0cb14e67054
Barbitch <154478533+barbmnz@users.noreply.github.com>
2024-05-15 03:13:58 -0400

    Update logica.js

--- logica.js
+++ logica.js
@@ -3,63 +3,154 @@ let carrito = [];
 
 const contenedorProds = document.getElementById('misprods');
 const tablaBody = document.getElementById('tablabody');
+const totalElement = document.getElementById('total');
+const botonVaciar = document.getElementById('vaciarBtn');
 
-function renderizarProductos(listaProds){
-    for(const prod of listaProds){
+function renderizarProductos(listaProds) { 
+    for (const prod of listaProds) {
         contenedorProds.innerHTML += `
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src=${prod.foto} alt=${prod.nombre}>
-        <div class="card-body">
-          <h5 class="card-title">${prod.nombre}</h5>
-          <p class="card-text">Precio: $ ${prod.precio}</p>
-          <button class="btn btn-primary compra" id=${prod.id}>Comprar</button>
+            <img class="card-img-top md-3" src=${prod.foto} alt=${prod.nombre}>
+            <div class="card-body">
+                <h5 class="card-title">${prod.nombre}</h5>
+                <p class="card-text">Precio: $ ${prod.precio}</p>
+                <input type="number" class="form-control cantidad" value="1" min="1">
+                <button class="btn btn-primary compra" id=${prod.id}>Comprar</button>
+            </div>
         </div>
-      </div>
-        `
+        `;
     }
 
     //eventos
     const botonesCompra = document.getElementsByClassName('compra');
-    for(const boton of botonesCompra){
+    for (const boton of botonesCompra) {
         //opcion 1 - addEventListener()
-        boton.addEventListener('click', ()=>{
-            console.log('Hiciste click en el boton cuyo id es: '+boton.id);
+        boton.addEventListener('click', () => {
+            console.log('Hiciste click en el boton cuyo id es: ' + boton.id);
             //buscar el objeto que tiene ese id
             const prodACarrito = listaProds.find(prod => prod.id == boton.id);
             console.log(prodACarrito);
+            // obtener la cantidad seleccionada
+            const cantidadSeleccionada = parseInt(boton.parentNode.querySelector('.cantidad').value);
+            // crear una copia del producto con la cantidad seleccionada
+            const productoConCantidad = { ...prodACarrito, cantidad: cantidadSeleccionada };
             //cargar el producto al carrito de compras
-            agregarACarrito(prodACarrito);
-        })
+            agregarACarrito(productoConCantidad);
+        });
     }
 }
 
 renderizarProductos(productosLista);
 
+function agregarACarrito(producto) {
+    // verificar si el producto ya está en el carrito
+    const productoExistenteIndex = carrito.findIndex(item => item.id === producto.id);
+    if (productoExistenteIndex !== -1) {
+        // si el producto ya está en el carrito, actualizar la cantidad
+        carrito[productoExistenteIndex].cantidad += producto.cantidad;
+    } else {
+        // si el producto no está en el carrito, agregarlo
+        carrito.push(producto);
+        Swal.fire({
+            title: "Genial",
+            text: `Agregaste: ${producto.nombre} al carrito`,
+            imageUrl: producto.foto,
+            imageWidth: 200,
+            imageHeight: 200,
+            imageAlt: "Custom image"
+          });
+    }
 
-function agregarACarrito(producto){
-    carrito.push(producto);
     console.table(carrito);
-    tablaBody.innerHTML+=`
-    <tr>
-        <td>${producto.id}</td>
-        <td>${producto.nombre}</td>
-        <td>${producto.precio}</td>
-    </tr>
-    `
-    const totalCarrito = calcularTotal(carrito);
-
-    const totalElement = document.getElementById('total');
-    totalElement.innerText = `Total a pagar $: ${totalCarrito}`;
+    renderizarCarrito();
 }
 
+function renderizarCarrito() {
+    tablaBody.innerHTML = '';
+
+    carrito.forEach((item, index) => {
+        tablaBody.innerHTML += `
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.nombre}</td>
+            <td>${item.precio}</td>
+            <td>${item.cantidad}</td>
+            <td><button class="btn btn-danger eliminar" data-index="${index}">Eliminar</button></td>
+        </tr>
+        `;
+    });
+
+    const totalCarrito = calcularTotal(carrito);
+    totalElement.innerText = `Total a pagar $: ${totalCarrito}`;
+
+    // Agregar eventos a los botones de eliminar
+    const botonesEliminar = document.querySelectorAll('.eliminar');
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener('click', () => {
+            const index = boton.getAttribute('data-index');
+            eliminarDelCarrito(index);
+        });
+    });
+}
+
+function eliminarDelCarrito(index) {
+    carrito.splice(index, 1);
+    renderizarCarrito();
+}
 
 // calcular el total de los productos en el carrito
 function calcularTotal(prodACarrito) {
     let total = 0;
     for (const producto of prodACarrito) {
-      total += producto.precio;
+        total += producto.precio * producto.cantidad;
     }
     return total;
+}
+
+const checkoutBtn = document.getElementById('checkoutBtn');
+
+checkoutBtn.addEventListener('click', () => {
+    realizarCheckout();
+});
+
+function realizarCheckout() {
+    if (carrito.length === 0) {
+        Swal.fire({
+            title: "No hay articulos en el carrito",
+            text: "Deber agregar productos para hacer Checkout!",
+            icon: "error"
+          });
+        return;
+    }
+
+    console.log('Checkout realizado');
+    limpiarCarrito();
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "success",
+        title: "¡Gracias por tu compra! Tu pedido ha sido procesado."
+      });
+}
+
+function limpiarCarrito() {
+    carrito = [];
+    tablaBody.innerHTML = '';
+    document.getElementById("total").innerText = 'Total a pagar: $ ';
+}
+
+botonVaciar.onclick = () => {
+    limpiarCarrito();
+    Swal.fire("Se han eliminado los productos del carrito");
 }
 
 const campoNombre = document.getElementById('nombre');
@@ -67,49 +158,46 @@ const campoEmail = document.getElementById('email');
 
 //opcion 2
 campoNombre.onkeyup = () => {
-   if(campoNombre.value.length < 3){
-    console.log('Nombre de menos de 3 letras 🚨');
-    campoNombre.style.color='red';
-   }else{
-    campoNombre.style.color='black';
-   }
+    if (campoNombre.value.length < 3) {
+        console.log('Nombre de menos de 3 letras 🚨');
+        campoNombre.style.color = 'red';
+    } else {
+        campoNombre.style.color = 'black';
+    }
 }
 //opcion 2
-campoNombre.onchange=()=>{
+campoNombre.onchange = () => {
     alert('Cambio el nombre en el formulario');
-    if(campoNombre.value == 'pepe'){
+    if (campoNombre.value == 'pepe') {
         campoNombre.value = '';
     }
 }
 
-campoEmail.addEventListener('input', ()=>{
-    if((!campoEmail.value.includes('@'))||(!campoEmail.value.includes('.'))){
-        document.getElementById('mensaje').innerText='Ingrese un email valido !'
-    }else{
-        document.getElementById('mensaje').innerText=''
+campoEmail.addEventListener('input', () => {
+    if ((!campoEmail.value.includes('@')) || (!campoEmail.value.includes('.'))) {
+        document.getElementById('mensaje').innerText = 'Ingrese un email valido !'
+    } else {
+        document.getElementById('mensaje').innerText = ''
     }
 })
 
-
 //opcion 3 que viene del html linea 66
-function borrarCampos(){
+function borrarCampos() {
     campoNombre.value = '';
     campoEmail.value = '';
 }
-
 
 //evento submit del formulario
 const formulario = document.getElementById('formulario');
 
 formulario.addEventListener('submit', validar);
 
-function validar(ev){
-    if((campoNombre.value == '') || (campoEmail.value == '')){
+function validar(ev) {
+    if ((campoNombre.value == '') || (campoEmail.value == '')) {
         ev.preventDefault();
         alert('Ingrese nombre o email faltante!');
     }
 }
-
 
 const contenedor = document.getElementById('principal');
 const boton = document.getElementById('mode');
@@ -123,7 +211,7 @@ boton.onclick = () => {
     }
 }
 
-// Funciones para cambiar el modo
+// funciones para cambiar el modo
 function pasarADark() {
     localStorage.setItem('mode', 'dark');
     boton.innerText = 'Light Mode';
@@ -138,9 +226,11 @@ function pasarALight() {
     document.body.className = 'light';
 }
 
-// Verificar el modo almacenado en localStorage al cargar la página
+// verificar el modo almacenado en localStorage al cargar la página
 if (localStorage.getItem('mode') === 'dark') {
     pasarADark();
 } else {
     pasarALight();
 }
+
+
